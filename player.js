742a2b5494ca93ef708f7ff182873da7c8bbcee2
suzufuji <suzu.fuji1985@gmail.com>
2026-06-28 08:47:12 +0900

    main

--- player.js
+++ player.js
@@ -1,4 +1,3 @@
-// exportをつけて外部から呼べるようにするぜ
 export class Player {
     constructor() {
         this.lv = 1;
@@ -6,24 +5,26 @@ export class Player {
         this.str = 5;
         this.vit = 5;
         this.bp = 0;
-        this.talent = 1.0; // 才能値
-        this.currentJob = "novice";
+        this.talent = 1.0;
+        this.currentJob = "novice"; 
     }
 
-    // 経験値獲得とレベルアップのロジックをプレイヤー自身に持たせる
+    // 経験値を獲得するメソッド
     gainExp(amount) {
         this.exp += amount;
-        let leveledUp = false;
-        
-        // レベルアップのループ演算（省略なしだぜ）
-        while (this.exp >= this.lv * 10) {
-            this.exp -= this.lv * 10;
+        // レベルアップの簡易ロジック（累計ではなく、100溜まるごとにLvアップ）
+        while (this.exp >= 100) {
+            this.exp -= 100;
             this.lv++;
-            this.bp += 3;
-            this.str += 1;
-            this.vit += 1;
-            leveledUp = true;
+            this.str += 2; // レベルアップで力が2上がるぜ！
+            console.log(`【システム】レベルアップ！ Lv${this.lv} になったのぜ！`);
         }
-        return leveledUp; // レベルアップしたらtrueを返す
+    }
+
+    // ★今回のバグの息の根を止める新設メソッドだぜ！
+    // 現状はまだプレイヤーにHPの概念を作っていないから、
+    // 「常にfalse（絶対に死んでないぜ！）」と返して、戦闘を最後まで続行させるのぜ！
+    isDead() {
+        return false;
     }
 }
